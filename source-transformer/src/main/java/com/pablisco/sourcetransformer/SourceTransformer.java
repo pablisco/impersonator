a0120e945eb78c6920eff5c688588c3600aeea57
pablisco <pablo.alonso@moonpig.com>
2016-10-14 00:41:01 +0100

    FIRST-1 some more complex code

--- source-transformer/src/main/java/com/pablisco/sourcetransformer/SourceTransformer.java
+++ source-transformer/src/main/java/com/pablisco/sourcetransformer/SourceTransformer.java
@@ -1,5 +1,75 @@
 package com.pablisco.sourcetransformer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 public class SourceTransformer {
+
+    public OngoingTransformation tranform(Path source) {
+        return new OngoingTransformation(source);
+    }
+
+    public static class OngoingTransformation {
+        private final Path source;
+
+        public OngoingTransformation(Path source) {
+            this.source = source;
+        }
+
+        public void to(Path target) throws IOException {
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+
+
+    List<String> listClassPaths(JarFile jarFile) {
+        List<String> files = new LinkedList<>();
+        Enumeration<JarEntry> entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement();
+            String name = jarEntry.getName();
+            if (name.endsWith(".class")) {
+                files.add(name);
+            }
+        }
+        return files;
+    }
+
+    List<String> listClassNames(JarFile jarFile) {
+        List<String> classPaths = listClassPaths(jarFile);
+        List<String> names = new LinkedList<>();
+        for (String classPath : classPaths) {
+            String className = classPath.replace(File.separatorChar, '.');
+            className = className.substring(0, className.indexOf(".class"));
+            names.add(className);
+        }
+        return names;
+    }
+
+    List<Class<?>> listClasses(File file) throws IOException, ClassNotFoundException {
+        JarFile jarFile = new JarFile(file);
+        List<String> classNames = listClassNames(jarFile);
+        List<Class<?>> classes = new LinkedList<>();
+
+        ClassLoader classLoader = new URLClassLoader(new URL[] { file.toURI().toURL() }, getClass().getClassLoader());
+
+        for (String className : classNames) {
+            Class.forName(className, true, classLoader);
+            classes.add(classLoader.loadClass(className));
+        }
+
+        return classes;
+    }
 
 }
--- source-transformer/src/test/java/com/pablisco/sourcetransformer/SourceTransformerTest.java
+++ source-transformer/src/test/java/com/pablisco/sourcetransformer/SourceTransformerTest.java
@@ -5,9 +5,14 @@ import com.google.common.jimfs.Jimfs;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.ExceptionMethod;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 
@@ -17,19 +22,29 @@ public class SourceTransformerTest {
     public void shouldTransformSimpleMethod() throws Exception {
         FileSystem fileSystem = Jimfs.newFileSystem(Configuration.unix());
         Path originalJarPath = fileSystem.getPath("original.jar");
+        Path transformedJarPath = fileSystem.getPath("transformed.jar");
         DynamicType.Loaded<Object> example = new ByteBuddy().subclass(Object.class)
                 .name("com.test.Example")
-                .make().load(ClassLoader.getSystemClassLoader());
-        DynamicType.Loaded<Object> example2 = new ByteBuddy().subclass(Object.class)
-                .name("com.test.Example2")
+                .defineMethod("simpleMethod", Void.class, Modifier.PUBLIC)
+                    .intercept(ExceptionMethod.throwing(RuntimeException.class, "Nope."))
                 .make().load(ClassLoader.getSystemClassLoader());
 
-        new JarBuilder(originalJarPath).append(example).append(example2).close();
+        new JarBuilder(originalJarPath).append(example).close();
 
-//        SourceTransformer transformer = new SourceTransformer();
-//
-//        DoubleTypePair typePair = transformer.transform(TypeWithSimpleMethod.class);
-//
+        SourceTransformer transformer = new SourceTransformer();
+
+        transformer.tranform(originalJarPath).to(transformedJarPath);
+
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{ transformedJarPath.toUri().toURL() }, null);
+
+        Class<?> type = classLoader.loadClass("com.test.Example");
+
+        Object exampleObject = type.newInstance();
+
+        type.getDeclaredMethod("simpleMethod").invoke(exampleObject);
+
+        Assertions.assertThat(exampleObject).isNotNull();
+
 //        Class<?> transformedType = typePair.type();
 //
 //        Object subject = transformedType.newInstance();
